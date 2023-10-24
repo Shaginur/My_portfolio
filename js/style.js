9504aa68f82430a24d7529bb6b760d3a123606f2
Mardamshi Shaginur <shaginu@gmail.com>
2023-10-24 16:36:38 +0500

    свайпер доработка

--- js/style.js
+++ js/style.js
@@ -1,1 +1,17 @@
+import Swiper from "swiper"; // Инициализация слайдера
+import { Navigation, Pagination } from "swiper/modules";
 
+const swiper = new Swiper(".swiper", {
+    modules: [Navigation, Pagination],
+    // Optional parameters
+    direction: "horizontal",
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
