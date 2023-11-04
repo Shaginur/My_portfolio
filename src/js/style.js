5dc4456dd4051a85a2b3b90e72f72b928bf52d24
Mardamshi Shaginur <shaginu@gmail.com>
2023-11-04 07:46:17 +0500

    добави рвзширения

--- src/js/style.js
+++ src/js/style.js
@@ -19,3 +19,22 @@ for (var i = 0; i < tabNavs.length; i++) {
     }
   })
 }
+
+import Swiper from 'swiper'
+import { Navigation, Pagination } from 'swiper/modules'
+
+const swiper = new Swiper('.mySwiper', {
+  modules: [Navigation, Pagination],
+  // Optional parameters
+  direction: 'horizontal',
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    type: 'progressbar',
+  },
+})
